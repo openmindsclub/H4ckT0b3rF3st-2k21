6f2113fcc6cba0118a1e0e0b561dad0fbc8a0ca6
Vedant Sharma <vedant.12sharma@gmail.com>
2021-10-13 09:13:11 +0530

    solved Euler challenge problem 49

---
+++ challenges/solving_project_euler/Eatbullets/Eatbullets_49.py
@@ -0,0 +1,24 @@
+import eulerLib as eulerlib
+
+
+def compute():
+	LIMIT = 10000
+	isprime = eulerlib.list_primality(LIMIT - 1)
+	for base in range(1000, LIMIT):
+		if isprime[base]:
+			for step in range(1, LIMIT):
+				a = base + step
+				b = a + step
+				if     a < LIMIT and isprime[a] and has_same_digits(a, base) \
+				   and b < LIMIT and isprime[b] and has_same_digits(b, base) \
+				   and (base != 1487 or a != 4817):
+					return str(base) + str(a) + str(b)
+	raise RuntimeError("Not found")
+
+
+def has_same_digits(x, y):
+	return sorted(str(x)) == sorted(str(y))
+
+
+if __name__ == "__main__":
+	print(compute())
