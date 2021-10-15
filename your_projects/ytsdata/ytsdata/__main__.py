3b9e63a3942630d28b10fd4f5ef5297cfa8455a8
Zaki Benaissa <zakben45@gmail.com>
2021-10-15 19:29:33 +0100

    Add download movie feature to ytsdata

--- your_projects/ytsdata/ytsdata/__main__.py
+++ your_projects/ytsdata/ytsdata/__main__.py
@@ -1,14 +1,6 @@
 ''' The Program's entry point where the CLI will run '''
 
-import yts_api
+from cli import run
 
 if __name__ == '__main__':
-    QUERY = ''
-
-    while True:
-        # Example: Keep asking the user to search
-        # for a movie and display results
-        QUERY = input('\n* Search for a movie: ')
-        results = yts_api.list_movies(query_term=QUERY)['movies']
-        for r in results:
-            print(f"{r['id']}-{r['title']}")
+    run()
